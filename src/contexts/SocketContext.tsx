88b02a7aa9db63e2b18e6cdbcd2510a65e1e4ab6
Anand Tripathi <anand.t9903@gmail.com>
2025-06-26 22:01:07 +0530

    Update SocketContext.tsx

--- src/contexts/SocketContext.tsx
+++ src/contexts/SocketContext.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 import { useAuth } from './AuthContext';
